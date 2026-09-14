d7832fa94876b138d0b42dfdb8a01452e5276920
Faith <faithakanga2404@gmail.com>
2026-09-14 15:11:35 +0100

    Modification of the port number and start command

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import messageRouter from "./routes/messageRoute.js"
 
+const PORT = process.env.PORT || 3000
+
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: true}))
 app.use("/", messageRouter)
-app.listen(3000)
+app.listen(PORT)
